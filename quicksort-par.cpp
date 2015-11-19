c665165bb5a9a70c5c2c12d0374e1d3fd596985d
Federico Menozzi <federicogmenozzi@gmail.com>
2015-11-19 13:02:29 -0500

    First crack at Cilk integration

--- quicksort-par.cpp
+++ quicksort-par.cpp
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 
-#include <omp.h>
+#include <cilk/cilk.h>
+#include <cilk/reducer_opadd.h>
 
 struct pair {
     int left;
@@ -18,29 +19,27 @@ double wctime() {
 int* prefix_sum(int X[], int lo, int hi) {
     int n = hi-lo+1;
 
-    int* Y = malloc(sizeof(*Y) * n/2);
-    int* S = malloc(sizeof(*S) * n);
-
-    int i;
+    int* Y = new int[n/2];
+    int* S = new int[n];
 
     S[0] = X[0];
 
     if (n > 1) {
-        for (i = 0; i < n/2; i++)
+        cilk_for (int i = 0; i < n/2; i++)
             Y[i] = X[2*i] + X[2*i+1];
         
         int* Z = prefix_sum(Y, lo, lo + (hi-lo)/2);
 
-        for (i = 1; i < n; i++)
+        cilk_for (int i = 1; i < n; i++)
             if (i % 2 == 1)
                 S[i] = Z[i/2];
             else
                 S[i] = Z[(i-1)/2] + X[i];
 
-        free(Z);
+        delete[] Z;
     }
 
-    free(Y);
+    delete[] Y;
 
     return S;
 }
@@ -50,31 +49,29 @@ struct pair partition(double A[], int lo, int hi) {
 
     int n = hi-lo+1;
 
-    int ltn = 0;
-    int eqn = 0;
-    int gtn = 0;
-
-    int i;
-
     // Determine the sizes of each partition
-    for (i = lo; i <= hi; i++)
+    cilk::reducer_opadd<double> ltn_reducer;
+    cilk::reducer_opadd<double> gtn_reducer;
+    cilk_for (int i = lo; i <= hi; i++)
         if (A[i] < Ap)
-            ltn += 1;
-    for (i = lo; i <= hi; i++)
+            ltn_reducer += 1;
+    cilk_for (int i = lo; i <= hi; i++)
         if (A[i] > Ap)
-            gtn += 1;
-    eqn = n-ltn-gtn;
+            gtn_reducer += 1;
+    int ltn = ltn_reducer.get_value();
+    int gtn = gtn_reducer.get_value();
+    int eqn = n-ltn-gtn;
 
     // Allocate partitions
-    double* lt = malloc(sizeof(*lt) * ltn);
-    double* eq = malloc(sizeof(*eq) * eqn);
-    double* gt = malloc(sizeof(*gt) * gtn);
+    double* lt = new double[ltn];
+    double* eq = new double[eqn];
+    double* gt = new double[gtn];
 
     // Populate masks
-    int* ltmask = malloc(sizeof(*ltmask) * n);
-    int* eqmask = malloc(sizeof(*eqmask) * n);
-    int* gtmask = malloc(sizeof(*gtmask) * n);
-    for (i = 0; i < n; i++) {
+    int* ltmask = new int[n];
+    int* eqmask = new int[n];
+    int* gtmask = new int[n];
+    cilk_for (int i = 0; i < n; i++) {
         ltmask[i] = A[lo + i] < Ap;
         gtmask[i] = A[lo + i] > Ap;
         eqmask[i] = !ltmask[i] && !gtmask[i];
@@ -86,7 +83,7 @@ struct pair partition(double A[], int lo, int hi) {
     int* gtsum = prefix_sum(gtmask, 0, n-1);
 
     // Populate lt, eq, and gt
-    for (i = 0; i < n; i++) {
+    cilk_for (int i = 0; i < n; i++) {
         if (ltmask[i])
             lt[ltsum[i]-1] = A[lo + i];
         if (eqmask[i])
@@ -96,26 +93,28 @@ struct pair partition(double A[], int lo, int hi) {
     }
 
     // Copy partitions into A
-    for (i = 0; i < ltn; i++)
+    cilk_for (int i = 0; i < ltn; i++)
         A[lo + i] = lt[i];
-    for (i = 0; i < eqn; i++)
+    cilk_for (int i = 0; i < eqn; i++)
         A[lo + ltn + i] = eq[i];
-    for (i = 0; i < gtn; i++)
+    cilk_for (int i = 0; i < gtn; i++)
         A[lo + ltn + eqn + i] = gt[i];
 
-    free(lt);
-    free(eq);
-    free(gt);
+    delete[] lt;
+    delete[] eq;
+    delete[] gt;
 
-    free(ltmask);
-    free(eqmask);
-    free(gtmask);
+    delete[] ltmask;
+    delete[] eqmask;
+    delete[] gtmask;
 
-    free(ltsum);
-    free(eqsum);
-    free(gtsum);
+    delete[] ltsum;
+    delete[] eqsum;
+    delete[] gtsum;
 
-    struct pair idxs = {lo + ltn - 1, lo + ltn + eqn};
+    struct pair idxs;
+    idxs.left  = lo + ltn - 1;
+    idxs.right = lo + ltn + eqn;
     return idxs;
 }
 
@@ -125,8 +124,10 @@ void quicksort(double A[], int lo, int hi) {
 
     struct pair p = partition(A, lo, hi);
 
-    quicksort(A, lo, p.left);
-    quicksort(A, p.right, hi);
+    cilk_spawn quicksort(A, lo, p.left);
+    cilk_spawn quicksort(A, p.right, hi);
+
+    cilk_sync;
 }
 
 int main(int argc, char* argv[]) {
@@ -146,7 +147,8 @@ int main(int argc, char* argv[]) {
         A[i] = (double)rand() / (double)RAND_MAX;
 
     start = wctime();
-    quicksort(A, 0, N-1);
+    cilk_spawn quicksort(A, 0, N-1);
+    cilk_sync;
     end = wctime();
 
     printf("%f\n", end-start);
