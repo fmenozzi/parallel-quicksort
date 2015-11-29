c705ca8943d76a0911175db373dd229134dd24e6
Federico Menozzi <federicogmenozzi@gmail.com>
2015-11-29 15:58:26 -0500

    Data collected on Phaedra

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-n=( 10 100 1000 10000 100000 1000000 10000000 100000000 )
+n=( 10 100 1000 10000 100000 1000000 10000000 100000000 1000000000 )
+
+touch seq-times.txt
+touch par-times.txt
 
 for i in "${n[@]}"
 do
-    touch seq-times-n-$i.txt
-    touch par-times-n-$i.txt
-
-	./seq $i >> seq-times-n-$i.txt
-	./par $i >> par-times-n-$i.txt
+    ./seq $i >> seq-times.txt
+    ./par $i >> par-times.txt
 done
